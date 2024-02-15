7fc97f72a695c21d9ed71041149be5262d878aa7
Juan cruz Leone <juancruzleone@outlook.com>
2024-02-15 13:00:51 -0300

    Se modifico la seccion quienes somos

--- front/src/pages/quienes-somos.jsx
+++ front/src/pages/quienes-somos.jsx
@@ -4,17 +4,18 @@ const quienesSomos = () => {
     return (
         <>
         <h1>Quiénes somos</h1>
-        <div className="contenedor-quienes-somos">
-            <p>¡Bienvenidos a <span>GreenPalate!</span></p>
-            <p>En GreenPalate, nos apasiona la alimentación consciente y saludable. Somos un equipo comprometido con la creación de un espacio en línea donde la cocina sin gluten, vegana, vegetariana y sin lactosa se convierte en una experiencia deliciosa y accesible para todos.</p>
-            <h2 className='titulos-quienes-somos'>Nuestra misión</h2>
-            <p>En el corazón de nuestro proyecto está la misión de fomentar un estilo de vida saludable y sostenible a través de la alimentación. Creemos en la importancia de ofrecer opciones culinarias que sean amigables con el medio ambiente, éticas y respetuosas con los animales.</p>
-            <h2>Únete a Nosotros en Este Viaje</h2>
-            <p>GreenPalate es más que una plataforma de recetas; es una comunidad comprometida con la salud, el bienestar y la conciencia alimentaria. Únete a nosotros en este emocionante viaje culinario hacia un estilo de vida más saludable y sostenible. 
+        <div className="contenedor-nuestra-historia">
+            <img src="equipo-trabajando.jpg" alt="Equipo trabajando" />
+            <div className='nuestra-historia'>
+                <h2 id='primer-h2'>Nuestra historia</h2>
+                <p>En GreenPalate, nos apasiona la alimentación consciente y saludable. Somos un equipo comprometido con la creación de un espacio en línea donde la cocina sin gluten, vegana, vegetariana y sin lactosa se convierte en una experiencia deliciosa y accesible para todos.</p>
+                <h2 className='titulos-quienes-somos'>Nuestra misión</h2>
+                <p>En el corazón de nuestro proyecto está la misión de fomentar un estilo de vida saludable y sostenible a través de la alimentación. Creemos en la importancia de ofrecer opciones culinarias que sean amigables con el medio ambiente, éticas y respetuosas con los animales.</p>
+                <h2>Únete a Nosotros en Este Viaje</h2>
+                <p>GreenPalate es más que una plataforma de recetas; es una comunidad comprometida con la salud, el bienestar y la conciencia alimentaria. Únete a nosotros en este emocionante viaje culinario hacia un estilo de vida más saludable y sostenible. 
             ¡Explora, crea y disfruta de la deliciosa revolución culinaria con GreenPalate!</p>
-        </div>
-       
-      
+            </div>
+        </div>  
         </>
     )
 }
